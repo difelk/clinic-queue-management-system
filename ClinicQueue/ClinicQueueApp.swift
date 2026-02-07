130fb907d5b97c2fc79ff5bc7e3e31cef79cfe52
difelk <dilshan.f@theavo.net>
2026-02-07 22:51:54 +0530

    project setup v1.01

---
+++ ClinicQueue/ClinicQueueApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClinicQueueApp.swift
+//  ClinicQueue
+//
+//  Created by dilshan fernando on 2026-02-07.
+//
+
+import SwiftUI
+
+@main
+struct ClinicQueueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
