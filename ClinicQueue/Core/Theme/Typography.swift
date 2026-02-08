ad50775803225d5e0bafbae4ec58958c3f56eb5c
difelk <dilshanfernando32@gmail.com>
2026-02-08 07:57:02 +0530

    Add Gilmer font files and set up typography

---
+++ ClinicQueue/Core/Theme/Typography.swift
@@ -0,0 +1,48 @@
+import SwiftUI
+
+enum AppTextStyle {
+    case heading
+    case subheading
+    case body
+    case paragraph
+    case caption
+
+    var size: CGFloat {
+        switch self {
+        case .heading: return 20
+        case .subheading: return 16
+        case .body: return 14
+        case .paragraph: return 13
+        case .caption: return 11
+        }
+    }
+
+    var weight: AppFontWeight {
+        switch self {
+        case .heading: return .bold
+        case .subheading: return .medium
+        case .body: return .regular
+        case .paragraph: return .regular
+        case .caption: return .regular
+        }
+    }
+}
+
+enum AppFontWeight {
+    case regular, medium, bold, heavy
+
+    var fontName: String {
+        switch self {
+        case .regular: return "Gilmer Regular"
+        case .medium:  return "Gilmer Medium"
+        case .bold:    return "Gilmer Bold"
+        case .heavy:   return "Gilmer Heavy"
+        }
+    }
+}
+
+extension Font {
+    static func app(_ style: AppTextStyle) -> Font {
+        .custom(style.weight.fontName, size: style.size)
+    }
+}
